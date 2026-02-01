d99f9e0d90d1a73e2d3c70fdace3989c4a9a86d6
Omar Jaramillo <33267882+Imperio1992@users.noreply.github.com>
2026-02-01 00:12:39 -0600

    Delete prestamos_je directory

--- prestamos_je/guardar_prestamo.php
+++
@@ -1,4 +0,0 @@
-$conexion->query("
-INSERT INTO caja(tipo,monto,concepto,id_empleado)
-VALUES('SALIDA',$monto_prestado,'Nuevo préstamo',$id_asesor)
-");
--- prestamos_je/menu.php
+++
@@ -1,5 +0,0 @@
-<div style="margin-bottom:20px;">
-    <a href="index.php" class="btn btn-secondary">
-        ⬅️ Volver al Inicio
-    </a>
-</div>
